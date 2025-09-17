2233305ea184640aa728613b4068acdb5b535271
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2025-09-17 13:31:08 -0300

    Organização de área adm

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from storage import models
+
 # Register your models here.
+
+
+@admin.register(models.Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "item_id",
+        "description",
+        "is_available",
+        "created_date",
+    )
+    ordering = ("-item_id",)
+    search_fields = ("item_id", "description")
+    list_per_page = 20
