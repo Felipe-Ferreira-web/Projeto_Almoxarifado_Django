49ec62a26f607d632cc8fd8c7cc30f99eb49fb9c
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2025-09-18 17:48:21 -0300

    Criação de owner

--- storage/models.py
+++ storage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,7 +12,7 @@ class Item(models.Model):
     storage_location = models.CharField(blank=True)
     is_available = models.BooleanField(default=True)
     created_date = models.DateTimeField(default=timezone.now)
-    #  owner_id = models.IntegerField()
+    owner_id = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f"{self.description}"
