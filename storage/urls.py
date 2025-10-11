8f1191a120ee127be5e3557cfc0d20fffe894211
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2025-10-10 22:37:45 -0300

    Criação da exibição idividual do item

--- storage/urls.py
+++ storage/urls.py
@@ -6,6 +6,8 @@ from django.conf import settings
 app_name = "items"
 
 urlpatterns = [
+    path("<int:item_id>/", views.item, name="item"),
+    path("search/", views.search, name="search"),
     path("", views.index, name="index"),
 ]
 
