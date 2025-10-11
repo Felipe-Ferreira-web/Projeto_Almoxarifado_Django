d47c1a68c4e050b5893aaadf7914379881a637c8
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2025-10-10 22:38:58 -0300

    Criação da funcionalidade da barra de pesquisa

--- storage/views/items_views.py
+++ storage/views/items_views.py
@@ -1,12 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, redirect
+from django.db.models import Q
 from storage.models import Item
 
 
 def index(request):
-    items = Item.objects.all()
 
-    context = {
-        "items": items,
-    }
+    items = Item.objects.order_by("-item_id")[:14]
+
+    context = {"items": items, "site_title": "Items - "}
+
+    return render(request, "storage/index.html", context)
+
+
+def item(request, item_id):
+
+    single_item = Item.objects.filter(pk=item_id).first()
+
+    context = {"item": single_item, "site_title": "Item - "}
+
+    return render(request, "storage/item.html", context)
+
+
+def search(request):
+
+    search_value = request.GET.get("q", "").strip()
+
+    if search_value == "":
+        return redirect("items:index")
+
+    items = Item.objects.filter(
+        Q(item_id__icontains=search_value)
+        | Q(name__icontains=search_value)
+        | Q(description__icontains=search_value)
+        | Q(storage_location__icontains=search_value)
+        | Q(created_date__icontains=search_value)
+    ).order_by("-item_id")
+
+    context = {"items": items, "site_title": "Search - "}
 
     return render(request, "storage/index.html", context)
