983f7deeedc0383534f30bec6358365fd875371a
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2025-11-02 16:59:23 -0300

    Adição dos urls de registro, login e logout

--- storage/urls.py
+++ storage/urls.py
@@ -13,6 +13,10 @@ urlpatterns = [
     path("items/create/", views.create, name="create"),
     path("items/<int:item_id>/update/", views.update, name="update"),
     path("items/<int:item_id>/delete/", views.delete, name="delete"),
+    # user
+    path("user/register/", views.register, name="register"),
+    path("user/login/", views.login_view, name="login"),
+    path("user/logout/", views.logout_view, name="logout"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
