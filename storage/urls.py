bbfbfdffe355d4a8b0ca811c143c674ad11312ad
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2025-11-23 17:02:51 -0300

    Adição de novos urls

--- storage/urls.py
+++ storage/urls.py
@@ -13,10 +13,14 @@ urlpatterns = [
     path("items/create/", views.create, name="create"),
     path("items/<int:item_id>/update/", views.update, name="update"),
     path("items/<int:item_id>/delete/", views.delete, name="delete"),
-    # user
+    # user (CRUD)
     path("user/register/", views.register, name="register"),
     path("user/login/", views.login_view, name="login"),
     path("user/logout/", views.logout_view, name="logout"),
+    path("user/update/", views.user_update, name="user_update"),
+    # user (view)
+    path("user/profile/<int:user_id>/detail/", views.user_profile, name="user_profile"),
+    path("user/profile/", views.user_owner_profile, name="user_owner_profile"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
