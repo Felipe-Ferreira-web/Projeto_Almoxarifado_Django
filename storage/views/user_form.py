c118ebb8ac00fd1d0b305b21b462d2ccd095b04a
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2025-12-09 16:06:14 -0300

    Adição de botões de atalhos entre páginas

--- storage/views/user_form.py
+++ storage/views/user_form.py
@@ -7,6 +7,22 @@ from django.contrib import auth
 
 
 def register(request):
+    """
+    View register new users.
+
+    Initializes RegisterForm. Handles POST submission by validating the data and saving
+    the new user object if validation succeeds.
+
+    Parameters:
+    ----------
+    request : HttpRequest
+        The HttpRequest object. Used to retrieve data about the context.
+    Returns:
+    -------
+    HttpResponse:
+        -Redirects to 'items:login' if the form validation succeeds (POST).
+        -Renders 'storage/register.html' and loads the context and the site_title (GET)
+    """
     form = RegisterForm()
 
     if request.method == "POST":
@@ -14,7 +30,7 @@ def register(request):
 
         if form.is_valid():
             form.save()
-            messages.success(request, "Usuário Registrado")
+            messages.success(request, "User registered successfully!")
             return redirect("items:login")
 
     return render(request, "storage/register.html", {"form": form})
@@ -22,7 +38,24 @@ def register(request):
 
 @login_required(login_url="items:login")
 def user_update(request):
+    """
+    View to update user data.
 
+    Requires a logged user. Initializes RegisterUpdateForm with the logged user's instance
+    for pre-population. Handles POST submission by validating the data and saving the
+    updated user object if validation succeeds.
+
+    Parameters:
+    ----------
+    request : HttpRequest
+        The HttpRequest object.
+    Returns:
+    -------
+    HttpResponse:
+        -Renders 'storage/user_update.html' with the form pre-populated with user data (GET).
+        -Renders 'storage/user_update.html' with the form and validation errors (Invalid POST).
+        -Rendirects to 'items:user_update' after successful update (Valid POST).
+    """
     form = RegisterUpdateForm(instance=request.user)
 
     if request.method != "POST":
@@ -34,11 +67,30 @@ def user_update(request):
         return render(request, "storage/user_update.html", {"form": form})
 
     form.save()
+    messages.success(request, "User updated sucessfully!")
 
     return redirect("items:user_update")
 
 
 def login_view(request):
+    """
+    View to login a existent user.
+
+    Initializes AuthenticationForm. Handles POST submission by validating the data,
+    authenticating the user, and logging them in if validation succeeds.
+    If the user is already authenticated, they are redirected to the index page..
+
+    Parameters:
+    ----------
+    request : HttpRequest
+        The HttpRequest object.
+    Returns:
+    -------
+    HttpResponse:
+        -Rendirects to 'items:index' after successful login (Valid POST).
+        -Renders 'storage/login.html' with error message if validation fails (POST).
+        -Renders 'storage/login.html' with the empty AuthenticationForm (GET).
+    """
     form = AuthenticationForm(request)
 
     if request.method == "POST":
@@ -57,5 +109,24 @@ def login_view(request):
 
 @login_required(login_url="items:login")
 def logout_view(request):
-    auth.logout(request)
-    return redirect("items:login")
+    """
+    View to logut the logged user.
+
+    Handles session termination by calling auth.logout(request). This action
+    should be accessed via POST for security (CSRF protection).
+
+    Parameters:
+    ----------
+    request : HttpRequest
+        The HttpRequest object.
+    Returns:
+    -------
+    HttpResponse:
+        -Rendirects to 'items:login' after successful logout (Valid POST).
+        -Redirects to 'items:user_profile' if the method is not POST (Security check).
+    """
+    if request.method == "POST":
+        auth.logout(request)
+        return redirect("items:login")
+
+    return redirect("items:user_profile", user_id=request.user.id)
