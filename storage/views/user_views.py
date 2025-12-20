d2b3dfe151b8e4f4bb86abaaaa859e217a5090ec
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2025-12-19 23:13:47 -0300

    refactor: remoção de função

--- storage/views/user_views.py
+++ storage/views/user_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from storage.models import Transaction
