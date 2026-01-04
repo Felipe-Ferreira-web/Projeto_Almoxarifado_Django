8bf28314c017c45166bd64afb285c9cee2e2f69f
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2026-01-04 00:21:23 -0300

    refactor: correção de nome de arquivo

--- utils/create_objects.py
+++ utils/create_objects.py
@@ -2,7 +2,7 @@ import os
 import sys
 from pathlib import Path
 import random
-from list_itens import objects
+from utils.list_items import objects
 import faker
 from faker.providers import BaseProvider
 import django
